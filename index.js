42d3522595317b592a9a06a30e339b49492bc22a
Soliunas <simas.soliunas@gmail.com>
2023-06-20 09:48:34 +0300

    Naujas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.clear();
 
 const input = '2665996659425501405540400873581907799489135464006142458778582491532721179559241333098879693925017533390586397561065764830808773222415309042678675337878431309';
 
-function dazniausiosPoros(number) {
+function dazniausiosPasikartojanciosPoros(number) {
     let skaiciuPoros = {};
     
     for (let i = 0; i < number.length - 1; i++) {
