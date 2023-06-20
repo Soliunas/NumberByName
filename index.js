47aa4c58607aedb392f86817e1796071004114a5
Soliunas <simas.soliunas@gmail.com>
2023-06-20 09:30:11 +0300

    Naujas

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 console.clear();
 
+const input = '2665996659425501405540400873581907799489135464006142458778582491532721179559241333098879693925017533390586397561065764830808773222415309042678675337878431309';
+const dazniausiaiPasikartojanciosPoros = dazniausiosPoros(input);
+
+function dazniausiosPoros(number) {
+    let skaiciuPoros = {};
+    
+    for (let i = 0; i < number.length - 1; i++) {
+      let pora = number[i] + number[i + 1];
+      
+      if (skaiciuPoros[pora]) {
+        skaiciuPoros[pora]++;
+      } else {
+        skaiciuPoros[pora] = 1;
+      }
+    }
+    
+    let dazniausiaiPasikartojancios = [];
+    let dazniausiaiPasikarktojancioss = 0;
+    
+    for (let pora in skaiciuPoros) {
+        let DazniausiosPoros = skaiciuPoros[pora];
+        
+      }
+    
+    return dazniausiaiPasikartojancios;
+  }
+  
+  console.log('Poros:', dazniausiaiPasikartojanciosPoros);
